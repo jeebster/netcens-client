764198e43e5bd146d311ce61465c4990a6bce3c7
Greg Morrison <freedomtoroam@posteo.de>
2021-12-17 00:44:55 -0500

    refactor(test): use basic App render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { create } from "react-test-renderer";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders the application successfully", () => {
+  expect(() => {
+    create(<App />);
+  }).not.toThrow();
 });
