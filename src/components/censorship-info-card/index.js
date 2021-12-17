c826803edc86d905e86777ff5eb8cc6f91716e12
Greg Morrison <freedomtoroam@posteo.de>
2021-12-16 20:01:50 -0500

    feat(components): add CensorshipInfoCard component

---
+++ src/components/censorship-info-card/index.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import "./index.css";
+
+const CensorshipInfoCard = ({ count, name }) => (
+  <div className="csic-container">
+    <h2>{name}</h2>
+    <div>{count.toLocaleString()} Measurements</div>
+  </div>
+);
+
+CensorshipInfoCard.propTypes = {
+  alpha2: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default CensorshipInfoCard;
---
+++ src/components/censorship-info-card/index.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CensorshipInfoCard from "./index";
+import { render, screen } from "@testing-library/react";
+
+test("CensorshipInfoCard rendering", () => {
+  const props = {
+    alpha2: "AU",
+    count: 12000,
+    name: "Australia",
+  };
+  const countPropRegex = new RegExp(props.count, "i");
+  const namePropRegex = new RegExp(props.name, "i");
+
+  render(<CensorshipInfoCard {...props} />);
+
+  const countPropElement = screen.getByText(countPropRegex);
+  expect(countPropElement).toBeInTheDocument();
+
+  const namePropElement = screen.getByText(namePropRegex);
+  expect(namePropElement).toBeInTheDocument();
+});
